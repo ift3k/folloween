580232c35329e0fc36667d7660d001155b63a8dc
tanj1m <tanjim1337@gmail.com>
2020-06-27 12:54:31 +0600

    Folloween front end

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,27 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+
+        <textarea
+            name="body"
+            class="w-full"
+            placeholder="What's up guys?"
+        ></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img
+                src="https://i.pravatar.cc/40"
+                alt="your avatar"
+                class="rounded-full mr-2"
+            >
+
+            <button
+                type="submit"
+                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
+            >
+                Tell something
+            </button>
+        </footer>
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,18 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/300"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">Ifte Kharul</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud.
+    </div>
+</div>
